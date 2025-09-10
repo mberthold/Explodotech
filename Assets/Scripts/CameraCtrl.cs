3be1e55b9b7dfe4485519ccd7c540ddbff17c5a1
mberthold <max-berthold@gmx.net>
2025-09-10 11:24:02 +0200

    Added simple zooming funcitonality - needs tuning.

---
+++ Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    private float zoom;
+    private float zoomMultiplier = 8f;
+    private float minZoom = 2f;
+    private float maxZoom = 800f;
+    private float velocity = 0f;
+    private float smoothTime = 0.25f;
+
+    [SerializeField] private Camera cam;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        zoom = cam.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        zoom -= scroll * zoomMultiplier;
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, zoom, ref velocity, smoothTime);
+    }
+}
